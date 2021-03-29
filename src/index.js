9fe358c080ee8933a1aa0f79121a228b5562ad42
Matthew Crooks <mfcrooks@gmail.com>
2021-03-29 22:03:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 function createStore(reducer) {
   let state;
 
-  return
-}
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
 
 function candyReducer(state = [], action) {
   switch (action.type) {
@@ -21,6 +33,7 @@ function render() {
     throw new Error("the store's state has not been defined yet")
   }
 };
+
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
